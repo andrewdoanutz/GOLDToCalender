dccbf3140a161042e5d37efcdfbe47f58a40ff46
andrewdoanutz <andrewdoanutz@gmail.com>
2019-01-12 15:59:43 -0800

    about us

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 import NavBar from './components/navbar';
 import Home from './home.js';
--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
+import Profile from './profile'
+
+
+import "../css/about.css";
+
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <div>about</div>
-        <div>about</div>
-        <div>about</div>
-        <div>about</div>
-        <div>about</div>
-        <div>about</div>
-        
+      
+      <div className="profiles">
+        <div className="andrew">
+        <Profile image="https://media.licdn.com/dms/image/C5603AQHaYrsskcDsfw/profile-displayphoto-shrink_800_800/0?e=1552521600&v=beta&t=iHRfLOcCBX2TKZRN4UcvFWu8Ndr7_NO0Zsfsk6_BEtw" 
+        name="Andrew Doan"
+        yearMajor="3rd Year Computer Engineer"
+        linkedIn="https://www.linkedin.com/in/andrewadoan/"
+        />
+        </div>
+        <div className="benson">
+        <Profile image="https://media.licdn.com/dms/image/C5603AQEqQFxxFRM8_w/profile-displayphoto-shrink_800_800/0?e=1552521600&v=beta&t=1e55cAZ29kbxtPYfIZXzTlUL0X1eT3fJ3PlcBlM7Qz8"
+        name="Benson Chu"
+        yearMajor="3rd Year Computer Science"
+        linkedIn="https://www.linkedin.com/in/benson-chu-566a40107/"
+        />
+        </div>
+        <div className="tim">
+        <Profile image="https://media.licdn.com/dms/image/C5603AQH5QVRBGzwu7A/profile-displayphoto-shrink_800_800/0?e=1552521600&v=beta&t=8U_ap4XUIBp0OL6scg75p2CPEuVCuY9oaDEY4n8JoZI"
+        name="Tim Chang"
+        yearMajor="3rd Year Computer Engineer"
+        linkedIn="https://www.linkedin.com/in/timchang2167/"
+        />
+        </div>
       </div>
     )
   }
