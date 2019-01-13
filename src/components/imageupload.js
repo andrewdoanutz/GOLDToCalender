8cc97c98aa5673916721790f748111359c3520e7
andrewdoanutz <andrewdoanutz@gmail.com>
2019-01-12 19:50:34 -0800

    image upload working

--- src/components/imageupload.js
+++ src/components/imageupload.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
+import ImageUploader from 'react-images-upload';
 
 export default class Upload extends Component {
-  render() {
-    return (
-      <div>
-        <div>up</div>
-        <div>up</div>
-        <div>up</div>
-        <div>up</div>
-        <div>up</div>
-        <div>up</div>
+  constructor(props) {
+    super(props);
+     this.state = { pictures: [] };
+     this.onDrop = this.onDrop.bind(this);
+}
 
-        <div>up</div>
-      </div>
-    )
-  }
+onDrop(picture) {
+    this.setState({
+        pictures: this.state.pictures.concat(picture),
+    });
+}
+
+render() {
+    return (
+        <ImageUploader
+            withIcon={false}
+            buttonText='Choose image'
+            onChange={this.onDrop}
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+            maxFileSize={5242880}
+        />
+    );
+}
 }
--- src/home.js
+++ src/home.js
@@ -2,20 +2,51 @@ import React, { Component } from 'react'
 import './css/home.css';
 import logo from './test2.png'
 import Proc from "./components/imageprocessor"
+import ImageUploader from 'react-images-upload';
+
 
 
 export default class Home extends Component {
- 
+  constructor(props) {
+    super(props);
+     this.state = { pictures: [],
+      imageuploaded:false };
+     this.onDrop = this.onDrop.bind(this);
+}
+
+onDrop(picture) {
+    this.setState({
+        pictures: this.state.pictures.concat(picture),
+        
+      imageuploaded:true
+    }
+    );
+    console.log("uploaded")
+}
+
 
     
 
 
         render() {
+          if(this.state.imageuploaded){
+            return(
+            <Proc image={this.state.pictures[0]}/>
+            )
+          }
+          else{
         return(
-
+          
           <div className="homebox">
-        
-         <Proc image={logo}/>
+          <ImageUploader
+            withIcon={true}
+            buttonText='Choose image'
+            onChange={this.onDrop}
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+            maxFileSize={5242880}
+        />
+        }
+         
          
          
         
@@ -25,6 +56,6 @@ export default class Home extends Component {
         }
         
       
-        
+      }
 
 }
