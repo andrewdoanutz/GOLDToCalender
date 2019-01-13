51866d3b65d10706892268dc8d1b9e1d89356bb1
andrewdoanutz <andrewdoanutz@gmail.com>
2019-01-12 19:08:26 -0800

    refactor tesseract

--- src/components/imageprocessor.js
+++ src/components/imageprocessor.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react'
 
-export default class Processor extends Component {
-  render() {
-    return (
-      <div>
-        <div>proc</div>
-        <div>proc</div>
-        <div>proc</div>
-        <div>proc</div>
-        <div>proc</div>
-        <div>proc</div>
-        <div>proc</div>
+
+
+
+var Tesseract=window.Tesseract;
+var wordsbool=false;
+var info="n/a";
+
+
+
+export default class Proc extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      words:"n/a",
+     
+    }
+  
+  };
+  
+
+componentDidMount(){
+  let self=this;
+  console.log("working");
+    Tesseract
+    .recognize(self.props.image)
+    .then(function(result){
+      console.log(result);
+      info=result.text;
+      console.log({info})
+      wordsbool=true;
+      self.setState({words:info});
+    })
+   
+    
+}
+
+        render() {
+         if(wordsbool===false){
+           return(
+             <div className="homebox">
+             Loading...
+             </div>
+             
+           )
+           
+          }
+         
+         else{
+          
+        return(
+
+          <div className="homebox">
         
-      </div>
-    )
-  }
+           
+         <div>result:</div>
+         <div>{this.state.words}</div>
+         
+         
+         
+        
+        
+       </div>
+          )
+        }
+        
+      }
+        
+
 }
--- src/home.js
+++ src/home.js
@@ -1,62 +1,21 @@
 import React, { Component } from 'react'
 import './css/home.css';
 import logo from './test2.png'
-
-
-var Tesseract=window.Tesseract;
-var wordsbool=false;
-var info="n/a";
-
+import Proc from "./components/imageprocessor"
 
 
 export default class Home extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      words:"n/a",
-     
-    }
-  
-  };
-  
+ 
 
-componentDidMount(){
-  let self=this;
-  console.log("working");
-    Tesseract
-    .recognize(logo)
-    .then(function(result){
-      console.log(result);
-      info=result.text;
-      console.log({info})
-      wordsbool=true;
-      self.setState({words:info});
-    })
-   
     
-}
+
 
         render() {
-         if(wordsbool===false){
-           return(
-             <div className="homebox">
-             Loading...
-             </div>
-             
-           )
-           
-          }
-         
-         else{
-          
         return(
 
           <div className="homebox">
         
-           
-         <div>result:</div>
-         <div>{this.state.words}</div>
-         
+         <Proc image={logo}/>
          
          
         
@@ -65,7 +24,7 @@ componentDidMount(){
           )
         }
         
-      }
+      
         
 
 }
